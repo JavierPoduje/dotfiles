ea83c0df2c4e31961934c54411326853fafed4f7
javierpoduje <javier.poduje@gmail.com>
2024-09-27 20:05:33 -0300

    fix: tmux issue

--- tmux/scripts/move-or-create.sh
+++ tmux/scripts/move-or-create.sh
@@ -11,27 +11,26 @@
 # - Switches to or creates session 3 if $1 is 3.
 # - Switches to or creates session 4 if $1 is 4.
 
-# TODO: Cosider the case in which we're moving to a non-existen session. eg:
-# current sessions: 1, 2, 3
-# remove session 2, leaving: 1, 3
-# go-to session 2.
-# current behavior: there are two sessions, so it tries to move to session 2, but it doesn't exist.
-# expected behaviour: create session 2 and move to it.
-
 NUMBER_OF_SESSIONS=$(tmux list-sessions | wc -l | xargs)
 
 if [ $1 = 1 ]; then
-  tmux switch -t "1"
-elif [ $1 = 2 ] && [ $NUMBER_OF_SESSIONS -lt 2 ]; then
-  tmux new -s 2 -d; tmux switch -t 2
-elif [ $1 = 2 ] && [ $NUMBER_OF_SESSIONS -ge 2 ]; then
-  tmux switch -t 2
-elif [ $1 = 3 ] && [ $NUMBER_OF_SESSIONS -lt 3 ]; then
-  tmux new -s 3 -d; tmux switch -t 3
-elif [ $1 = 3 ] && [ $NUMBER_OF_SESSIONS -ge 3 ]; then
-  tmux switch -t 3
-elif [ $1 = 4 ] && [ $NUMBER_OF_SESSIONS -lt 4 ]; then
-  tmux new -s 4 -d; tmux switch -t 4
-elif [ $1 = 4 ] && [ $NUMBER_OF_SESSIONS -ge 4 ]; then
-  tmux switch -t 4
+    tmux switch -t "1"
+elif [ $1 = 2 ]; then
+    if tmux has-session -t 2 2>/dev/null; then
+        tmux switch -t 2
+    else
+        tmux new -s 2 -d; tmux switch -t 2
+    fi
+elif [ $1 = 3 ]; then
+    if tmux has-session -t 3 2>/dev/null; then
+        tmux switch -t 3
+    else
+        tmux new -s 3 -d; tmux switch -t 3
+    fi
+elif [ $1 = 4 ]; then
+    if tmux has-session -t 4 2>/dev/null; then
+        tmux switch -t 4
+    else
+        tmux new -s 4 -d; tmux switch -t 4
+    fi
 fi
