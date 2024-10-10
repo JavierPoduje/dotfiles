9228c879348810d0cfd3961e90e238e87b849992
javierpoduje <javier.poduje@gmail.com>
2024-10-10 16:01:32 -0300

    nvim: add openingh

---
+++ nvim/lua/plugins/openingh.lua
@@ -0,0 +1,13 @@
+return {
+	"almo7aya/openingh.nvim",
+	config = function()
+		-- :OpenInGHRepo
+		-- Opens the project's git repository page in GitHub.
+
+		-- :OpenInGHFile
+		-- Opens the current file page in GitHub. This command supports ranges.
+
+		-- :OpenInGHFileLines
+		-- Opens the current file page in GitHub. This command supports ranges.
+	end,
+}
