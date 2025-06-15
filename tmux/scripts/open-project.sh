c8253bc309e45a86d7d44b78d4cabfd12cc1e678
javierpoduje <javier.poduje@gmail.com>
2025-06-15 12:24:53 +0200

    sh: improve `pilla`

--- tmux/scripts/open-project.sh
+++ tmux/scripts/open-project.sh
@@ -4,7 +4,13 @@
 projects_folder="$HOME/Documents/projects"
 
 # Capture the result
-selected_path=$(find "$projects_folder" -type d -not -path "*/node_modules/*" -not -path "*/.git/*" | sed "s|^$(echo "$projects_folder" | sed 's/\./\\./g')/||" | fzf)
+selected_path=$(
+  find "$projects_folder" -type d \
+    -name ".git" -prune \
+    -not -path "*/node_modules/*" \
+  | sed "s|^$(echo "$projects_folder" | sed 's/\./\\./g')/||" | sed 's/\/.git//' \
+  | fzf
+)
 
 # You can then use 'selected_path' in your script
 if [ -n "$selected_path" ]; then
@@ -15,16 +21,14 @@ if [ -n "$selected_path" ]; then
   # Check if a tmux server is running and attached
   if tmux has-session 2>/dev/null; then
     echo "Tmux session detected. Creating new windows."
-    # Create a new window named "cli" and send it to the selected directory
-    cd "$selected_project_path"
     # tmux new-window -n "cli"
     tmux rename-window "cli"
+    tmux send-keys -t "cli" "cd $selected_project_path" Enter
 
     # 2. Open another window called "code" and open neovim in it.
     # Create another new window named "code", change to the directory, and start nvim
     tmux new-window -n "code"
-
-    tmux send-keys -t "code" "nvim" Enter
+    tmux send-keys -t "code" "cd $selected_project_path && nvim" Enter
   else
     echo "No active tmux session found. Starting a new one."
   fi
