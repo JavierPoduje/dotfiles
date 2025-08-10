0c35b3651f19e5064ceaf25ccd75a92d60e02d0b
javierpoduje <javier.poduje@gmail.com>
2025-08-10 21:02:46 +0200

    nvim: update smartsort

--- nvim/lua/plugins/smartsort.lua
+++ nvim/lua/plugins/smartsort.lua
@@ -1,18 +1,18 @@
 return {
     "smartsort.nvim",
     dev = true,
+    lazy = false,
     config = function()
-        local setup = '{non_sortable_behavior = "preserve",separator = ","}'
+        local setup = '{non_sortable_behavior = "preserve"}'
 
         require('smartsort').setup({
             non_sortable_behavior = "preserve",
-            separator = ",",
+            single_line_separator = ",",
         })
 
         vim.keymap.set("v", "<leader>0", ":Smartsort")
         vim.keymap.set("n", "<leader>9", ":SReload<cr>")
         vim.keymap.set("v", "<leader>8", ":SPrintChadnode<cr>")
-        vim.keymap.set("v", "<leader>7",
-            ":'<,'>lua require('smartsort').smartsort(" .. setup .. ")")
+        vim.keymap.set("v", "<leader>7", ":'<,'>lua require('smartsort').smartsort(" .. setup .. ")")
     end
 }
